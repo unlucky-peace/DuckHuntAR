482cf7700aa6edde63097791a9c5500e552a7931
unlucky-peace <51030011+unlucky-peace@users.noreply.github.com>
2021-09-30 14:33:33 +0900

    스테이지 사이 애니메이션

--- Assets/Scripts/StageAnimation.cs
+++ Assets/Scripts/StageAnimation.cs
@@ -7,6 +7,7 @@ public class StageAnimation : MonoBehaviour
 {
     public TextMeshProUGUI stageText;
     private float sleepTime = 0.3f;
+    private string stage = "STAGE ";
     
     public void StageAnimationStart()
     {
@@ -18,18 +19,13 @@ public class StageAnimation : MonoBehaviour
     {
         yield return new WaitForSeconds(1.5f);
         //이거 이렇게까지 해야하나 누가 도움좀
-        stageText.text += "S";
-        yield return new WaitForSeconds(sleepTime);
-        stageText.text += "T";
-        yield return new WaitForSeconds(sleepTime);
-        stageText.text += "A";
-        yield return new WaitForSeconds(sleepTime);
-        stageText.text += "G";
-        yield return new WaitForSeconds(sleepTime);
-        stageText.text += "E";
-        yield return new WaitForSeconds(sleepTime);
-        stageText.text += " ";
-        yield return new WaitForSeconds(sleepTime);
+
+        for (int i = 0; i < stage.Length; i++)
+        {
+            stageText.text += stage[i];
+            yield return new WaitForSeconds(sleepTime);
+        }
+        
         stageText.text += GameManager.Instance.stage;
         yield return new WaitForSeconds(sleepTime + 1);
         stageText.text = "";
